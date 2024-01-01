e44431c0c1f520c4cd6b3b6de5f97c3d3ce06b9e
Oksana <91112565+Volovenk0@users.noreply.github.com>
2024-01-01 21:14:14 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,23 +1,26 @@
--- 3.1 Вивести кількість продажів книг за жанром
-select genre, sum(sales.approximate_sales_in_millions) as total_sales_in_millions 
-	from book join sales 
-		on book.num_sale_id = sales.num_sale_id
-group by book.genre
-order by total_sales_in_millions desc;
+-- 3.1 Вивести жанри книг за спаданням їх рейтингу
+select max(book.rating) as max_rating, genre.genre_name 
+	from book join book_genre
+		on book.book_id = book_genre.book_id
+			join genre
+				on book_genre.genre_id = genre.genre_id
+group by genre.genre_name
+order by max_rating desc;
 
--- 3.2 Вивести топ 5 авторів за кількістю продажів книг
-select author.author_name, sum(sales.approximate_sales_in_millions) as total_sales_in_millions 
-	from author join book 
-		on author.author_name = book.author_name
-			join sales 
-				on book.num_sale_id = sales.num_sale_id
+-- 3.2 Вивести топ 5 авторів за рейтингом книг
+select author.author_name, max(book.rating) as max_rating
+	from author join book_author
+		on author.author_id = book_author.author_id
+			join book
+				on book_author.book_id = book.book_id
 group by author.author_name
-order by total_sales_in_millions desc
+order by max_rating desc
 limit 5;
 
--- 3.3 Вивести кількість продажів книг за мовою оригіналу
-select book.original_language, sum(sales.approximate_sales_in_millions) as total_sales_in_millions
-	from book join sales
-		on book.num_sale_id = sales.num_sale_id
-group by book.original_language
-order by total_sales_in_millions desc; 
+-- 3.3 Вивести жанри книг з ціною менше 10$ і рейтингом більше 4.5
+select genre.genre_name 
+	from book join book_genre
+		on book.book_id = book_genre.book_id
+			join genre
+				on book_genre.genre_id = genre.genre_id
+where book.price < 10 and book.rating > 4.5;
